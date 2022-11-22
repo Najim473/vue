db07dddad0086bef6db3eb0a90afb887524bb2d2
Najim Uddin <najim7304@gmail.com>
2022-11-22 13:30:09 +0600

    learn vue script

---
+++ src/components/button/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <button @click="$emit('buttonClicked')">{{text}}</button> -->
+    <button @click="handleButtonClicked">{{text}}</button>
+    <!-- <button>{{props.text}}</button> -->
+</template>
+<script setup>
+const props = defineProps({
+    text:{
+        type:String,
+        default:'No text specified'
+    }
+})
+// console.log(props.text)
+const emit = defineEmits(['buttonClicked'])
+const handleButtonClicked = ()=>{
+    emit('buttonClicked','react')
+}
+</script>
+
+<!-- <script>
+export default{
+    props:{
+        text:{
+            type: String,
+            default:'No text specified'
+        }
+    },
+    emits:['buttonClicked']
+}
+</script> -->
