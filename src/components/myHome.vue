3c9f984dfcd0dde01e0bbe85c1b21a37aaea3ff3
Najim Uddin <najim7304@gmail.com>
2022-12-13 13:44:34 +0600

    modified

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
- <h1>What is your favorite fruits</h1>
- <p><input v-model="fruits"></p>
- <p v-once="fruits">{{fruits}}</p>
- <span v-pre>My favorite fruits is {{fruits}}</span>
- <pre>{{$data}}</pre>
+    <h1>What is your favorite fruits</h1>
+    <p><input v-model="fruits" /></p>
+    <p v-once="fruits">{{ fruits }}</p>
+    <span v-pre>My favorite fruits is {{ fruits }}</span>
+    <pre>{{ $data }}</pre>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-       fruits :'I like to eat Apple'
+      fruits: "I like to eat Apple",
     };
   },
 };
