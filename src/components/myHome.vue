a694996523f4cc9784ac78ba5b7547af3b02d01e
Najim Uddin <najim7304@gmail.com>
2022-12-13 14:09:24 +0600

    v-html and v-text

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
- <div class="item">
-  <div class="quantity">
-    <button class="incre" @click="counter>0 ? counter-=1:0">-</button>
-    <span>count: {{counter}}</span>
-    <button class="incre" @click=" counter+=1 ">+</button>
-
-  </div>
- </div>
+    <!-- v-html  -->
+ <h3>What is your favorite kind of tacos?</h3>
+ <!-- <p v-html="tacos"></p> -->
+ <!-- v-text  -->
+ <p v-text="tacos"></p>
+ <p>{{tacos}}</p>
+ <p><input v-model="tacos"></p>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-     counter:0
+      // v-html 
+      // tacos:`I like <a href="najimuddin.me" onclick="alert('I hacked you and stole all your tacos haha')">Al Pastor</a>`
+      // tacos:`I like <a href="najimuddin.me">Al Pastor</a>`
+      // v-text 
+      tacos:'I like came asada'
     };
   },
 };
