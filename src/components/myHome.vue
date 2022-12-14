3b7642a416a269c82c19339f877226dbf9fa8503
Najim Uddin <najim7304@gmail.com>
2022-12-14 20:58:39 +0600

    sorting table data with v-for

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,57 +1,50 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name:</label>
-      <br />
-      <input type="text" id="name" v-model="name" required />
-    </div>
-    <div>
-      <label for="email">Email:</label>
-      <br />
-      <input type="email" id="email" v-model="email" required />
-    </div>
-    <div>
-      <label for="caps">HOW DO I TURN OFF CAPS LOCK</label>
-      <textarea
-        id="caps"
-        cols="30"
-        rows="10"
-        v-model="caps"
-        required
-      ></textarea>
-    </div>
-    <button :class="[name ? activeClass : '']" type="submit">submit</button>
-    <div>
-      <h5>Response from server:</h5>
-      <pre>{{ response }}</pre>
-    </div>
-  </form>
+  <h3>
+    Sort titles by:
+    <button @click="sortLowest">Lowest Rated</button>
+    <button @click="sortHighest">Highest Rated</button>
+  </h3>
+
+    <table>
+      <thead>
+        <tr>
+          <th v-for="key  in columns">
+            {{ key }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entry in ratingInfo">
+          <td v-for="key in columns">
+            {{entry[key]}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
 </template>
 <script>
 export default {
   data() {
     return {
-      name: "",
-      email: "",
-      caps: "",
-      response: "",
-      activeClass: "active",
+      columns: ["title", "rating"],
+      ratingInfo: [
+        { title: "White Chicks", rating: 82 },
+        { title: `Grey's anatomy`, rating: 98 },
+        { title: "Person Break", rating: 93 },
+        { title: "How I Net Your Mother", rating: 94 },
+        { title: "Supernatural", rating: 85 },
+        { title: "Breaking Bed", rating: 91 },
+        { title: "Death Note", rating: 97 },
+        { title: "Naruto", rating: 83 },
+      ],
     };
   },
   methods: {
-    submitForm() {
-      axios
-        .post("https://jsonplaceholder.typicode.com/posts", {
-          name: this.name,
-          email: this.email,
-          caps: this.caps,
-        })
-        .then((response) => {
-          this.response = JSON.stringify(response, null, 2);
-        })
-        .catch((error) => {
-          this.response = "Error:" + error.response.status;
-        });
+    sortLowest() {
+      this.ratingInfo.sort((a, b) => (a.rating > b.rating ? 1 : -1));
+    },
+    sortHighest() {
+      this.ratingInfo.sort((a, b) => (a.rating < b.rating ? 1 : -1));
     },
   },
 };
