665bec229642063f309b065999d82bdd3adae48c
Najim Uddin <najim7304@gmail.com>
2022-12-18 15:31:01 +0600

    prop types and validations

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -4,26 +4,34 @@
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem in
       vero ex esse error ducimus, facere omnis tempora obcaecati officiis!
     </p>
-    <compo name="Mango" name2="Apple"/>
-    <compo name="Apple" name2="Mango"/>
-    <compo name="Banana" name2="Apple"/>
-    <compo :name="name" :name2="name2"/>
+    <compo name="Mango" name2="Apple" />
+    <compo name="Apple" name2="Mango" />
+    <compo name="Banana" name2="Apple" />
+    <compo :name="name" :name2="name2" />
+    <br />
+    <br />
+    <br />
+    <!-- <arti /> -->
+    <!-- <arti title="Article" :like="20" /> -->
+    <arti :like="20" :isPublished="false" />
   </div>
 </template>
- <script>
- import compo from './component.vue'
-export default{
-  name:'myHome',
- components:{
-  compo
- },
- data(){
-  return{
-    name: 'Lemon',
-    name2:'Orange'
-  }
- }
-}
+<script>
+import compo from "./component.vue";
+import arti from "./Article.vue";
+export default {
+  name: "myHome",
+  components: {
+    compo,
+    arti,
+  },
+  data() {
+    return {
+      name: "Lemon",
+      name2: "Orange",
+    };
+  },
+};
 </script>
 <!-- <style lang="scss" scoped>
 @import "./myHome.scss";
