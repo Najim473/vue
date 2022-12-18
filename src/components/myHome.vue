5014620339c15fa92b589a173f2c6500873be2d1
Najim Uddin <najim7304@gmail.com>
2022-12-18 11:46:03 +0600

    watchers exercise

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,84 +1,35 @@
-
-<!-- BEER COUNTING  -->
 <template>
- <div v-for="beer in beers">
-  <h1>{{beer.name}} </h1>
- </div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      bottom: false,
-      beers: []
-    };
-  },
-  watch: {
-    bottom(newValue) {
-      if (newValue) {
-        this.addBeer();
-      }
-    }
-  },
-  created() {
-    window.addEventListener("scroll", () => {
-      this.bottom = this.bottomVisible();
-    });
-    this.addBeer();
-  },
-  methods: {
-    bottomVisible() {
-      const scrollY = window.scrollY;
-      const visible = document.documentElement.clientHeight;
-      const pageHeight = document.documentElement.scrollHeight;
-      const bottomOfPage = visible + scrollY >= pageHeight;
-      return bottomOfPage || pageHeight < visible;
-    },
-    addBeer() {
-      axios.get("https://api.punkapi.com/v2/beers/random").then((response) => {
-        let api = response.data[0];
-        let apiInfo = {
-          name: api.name,
-          desc: api.description,
-          img: api.image_url,
-          tips: api.brewers_tips,
-          tagline: api.tagline,
-          food: api.food_pairing
-        };
-        this.beers.push(apiInfo);
-        if (this.bottomVisible()) {
-          this.addBeer();
-        }
-      });
-    }
-  }
-};
-</script>
-<style lang="scss" scoped>
-@import "./myHome.scss";
-</style>
-
-<!-- COUNTING WATCHER CHANGE  -->
-<!-- <template>
-  <div>
-    <input type="number" v-model.number="counter" />
+  <div class="main">
+ <button @click="getTaco">Click to order a taco</button>
+ <br>
+ <br>
+ <br>
+ <h2>{{orderStatus}}</h2>
+ <p v-if="orderSubmitted">{{orderSubmitted}}</p>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      counter: 0,
-      bottom: false,
-      beers: [],
+  orderStatus:'Where is taco...',
+  orderSubmitted:null
     };
   },  
+  methods:{
+    getTaco(){
+      this.orderStatus = '🌮!'
+    }
+  },
   watch: {
-    counter(newvalue, oldValue) {
-      console.log(
-        `The counter has changed! It was old ${oldValue} and It's new value ${newvalue}`
-      );
-    },
+   orderStatus(newValue,oldValue){
+this.orderSubmitted = 'your order was submitted !'
+setTimeout(() => this.orderSubmitted=null,1000);
+   }
   },
 };
-</script> -->
+</script>
+<style lang="scss" scoped>
+@import "./myHome.scss";
+</style>
+
