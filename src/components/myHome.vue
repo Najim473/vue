3bebbbacbb7c26d63591b42f1f8ba4e0631018fb
Najim Uddin <najim7304@gmail.com>
2022-12-19 12:26:37 +0600

    slots_props

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,37 +1,21 @@
 <template>
   <div class="main">
-    <!-- <card content="content 01"/>
-    <card content="content 02"/> -->
-    <card/>
-    <card>Card content</card>
-    <card><h2>Card content</h2></card>
-    <card>
-      <img
-        src="https://i.ibb.co/zRj6zwP/pexels-anna-shvets-4588065.jpg"
-        alt=""
-      />
-    </card>
-    <!-- NAMED SLOTS  -->
- <card>
-  <template v-slot:header>
-    Header
+ <NameList>
+  <template v-slot:default="slotProps">
+    {{slotProps.firstName}}
+    <br>
+    <br>
+    {{ slotProps.lastName }}
   </template>
-  <template v-slot:default>
-    <img src="https://i.ibb.co/3hcHzsg/pexels-kelvin-valerio-617278.jpg" alt="">
-  </template>
-  <template v-slot:footer>
-    <button>View details</button>
-  </template>
-
- </card>
+ </NameList>
   </div>
 </template>
 <script>
-import card from "./card.vue";
+import NameList from "./NameList.vue";
 export default {
   name: "myHome",
   components: {
-    card,
+    NameList,
   },
   data() {
     return {};
